0180e1a783fdbcfd15606f6c6204ff2af2187e32
rocco <rocco@roccosportal.com>
2015-06-24 17:47:40 +0200

    basic setup

--- wsid.go
+++ wsid.go
@@ -1,7 +1,112 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+)
+
+type thing struct{
+ id       sql.NullInt64
+ name sql.NullString
+
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	args := os.Args[1:]
+
+	command := "go"
+	if len(args) >= 1 {
+		command = args[0]
+	}
+
+	db := connect()
+	defer db.Close()
+
+	setup(db)
+	if command == "add" {
+		add(db, args)
+	} else if command == "go" {
+		gogo(db)
+	} else if command == "show" {
+		show(db)
+	} else {
+		fmt.Println("Command not found. Try help.")
+	}
+}
+
+// adds a thing
+func add(db  *sql.DB, args[]string ) {
+	if len(args) != 2 {
+		fmt.Println("There is nothing to add.")
+		return
+	}
+	name := args[1]
+
+	_, err := db.Exec("insert into things(name) values(?)", name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Added: ", name)
+}
+
+// removes a thing
+func remove() {
+
+}
+
+func show(db  *sql.DB) {
+	rows, err := db.Query("select id, name from things")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		rows.Scan(&id, &name)
+		fmt.Println(id, name)
+	}
+	rows.Close()
+}
+
+
+func gogo(db  *sql.DB) {
+	rows, err := db.Query("SELECT * FROM things ORDER BY RANDOM() LIMIT 1;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	if rows.Next(){
+
+		var id int
+		var name string
+		rows.Scan(&id, &name)
+		fmt.Println("You should do:",name)
+	} else {
+		fmt.Println("There are no things you can do!")
+	}
+
+	rows.Close()
+}
+
+func  connect() (db  *sql.DB) {
+	db, err := sql.Open("sqlite3", "./wsid.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func setup(db *sql.DB){
+	sqlStmt := `
+	create table IF NOT EXISTS things (id integer not null primary key, name text);
+	`
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
 }
